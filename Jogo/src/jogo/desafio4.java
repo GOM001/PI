0ba7f2330434f1ec727f26c86240727987576030
lacceleratorl <55900270+lacceleratorl@users.noreply.github.com>
2019-11-02 15:49:57 -0300

    Update desafio4.java

--- Jogo/src/jogo/desafio4.java
+++ Jogo/src/jogo/desafio4.java
@@ -3,19 +3,31 @@ package jogo;
 public class desafio4 {
 	public static void main(String[] args) {
 		System.out.println("   \n              "
-				+ " ▄█████████▄▄▄\r\n" + 
-				"               █           █\r\n" + 
-				"               █    █   █  █\r\n" + 
-				"               █      █    █\r\n" + 
-				"               ▒▒▒▒▒▒▒▒▒▒▒▒▒\r\n" + 
-				"              ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒\r\n" + 
-				"             ▄█             █▄\r\n" + 
-				"           ▄▀ █       ▀      █▀▄\r\n" + 
-				"      ▀▀▀▄▀   █              █  ▀▄▀▀▀ \r\n" + 
-				"       ▄▀ █   █       ▀      █  █ ▀▄\r\n" + 
-				"          ▀   █              █  ▀\r\n" + 
-				"              █       ▀      █\r\n" + 
-				"              ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀\r\n" + 
+				+"              _________________________________________________________________________________________________                                                                                                           \n"+
+				"                           |                                                                                                 |  \n"+
+				"                           |   Ola alma perdida, voce veio meio socorrer? estou perdido no deserto e preciso voltar ao meu   |       \n"+
+				"                           | habitart de neve. Se consegui me ajudar ira continuar sua jornada a sua recompensa,             |             \n"+
+				"                           | caso contrario eu irei desaparecerei e vc ficara nesse deserto por toda a eternidade.           |        \n"+
+				"                           | a Charada e encontre a resposta de em quanto tempo irei derreter e o guardiao das almas         |             \n"+
+				"                           | ira permitir a nossa passagem para meu habitart.                                                |          \n"+
+				"                           |  _______________________________________________________________________________________________|          \n"+
+				"                           /                                                                                                             \n"+
+				"                ▄▄▄▄▄▄▄▄▄                         Volume             /                                                                                   \n"+
+				"                █████████                      2.400  |-------------/                                                                                              \n"+
+				"               ███████████▄▄                          |            / |                                                                                \n" + 
+				"               █░░░░░░░░░░░█                          |           /  |                                                                   \r\n" + 
+				"               █░░░░█░░░█░░█                          |          /   |                                                               \r\n" + 
+				"               █░░░░░░█░░░░█                    2.000 |---------/    |                                                               \r\n" + 
+				"               ▓▓▓▓▓▓▓▓▓▓▓▓▓▓                         |       / |    |                                                                  \r\n" + 
+				"              ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓                        |      /  |    |                                                                 \r\n" + 
+				"             ▄█░░░░▓▓▓░▓▓▓░░░█▄                       |     /   |    |                                                                 \r\n" + 
+				"           ▄▀ █░░░░▓▓░▀░▓▓░░░█▀▄                      |    /    |    |                                                                \r\n" + 
+				"      ▀▀▀▄▀   █░░░░░░░░░░░░░░█  ▀▄▀▀▀                 |   /     |    |                                                               \r\n" + 
+				"       ▄▀ █   █░░░░░░░▀░░░░░░█  █ ▀▄                  |  /      |    |                                                             \r\n" + 
+				"          ▀   █░░░░░░░░░░░░░░█  ▀                     | /       |    |                                                         \r\n" + 
+				"              █░░░░░░░▀░░░░░░█                  1.000 |/        |    |                                 \r\n" + 
+				"              ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀                    ____|_________|____|_______________                                                                          \r\n" + 
+				"                                                     0|        50    75         Tempo                                                                                 \n"+
 				"");
 	}
 
