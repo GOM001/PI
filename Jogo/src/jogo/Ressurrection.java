f809cc3f405a0f30215c5bec7225e7d643845e23
felipe.amsilva5 <felipe.amsilva5@CAS527236W1001.senacsp.edu.br>
2019-11-05 19:42:33 -0300

    adicionado boneco de neve e correção da aranha

--- Jogo/src/jogo/Ressurrection.java
+++ Jogo/src/jogo/Ressurrection.java
@@ -132,9 +132,9 @@ public class Ressurrection {
 			System.out.println("\n"
 					+"                           ______________________________________________________________________                                             \n"+
 					"                          |                                                                      |\n"+
-					 "                         | - Olá criatura desprezível, está preparado para  uma morte terrível? |                                             \n" + 
+					 "                          | - Olá criatura desprezível, está preparado para  uma morte terrível? |                                             \n" + 
 					"                          |    Sou a KumoKumo  mas pode me chamar de presságio do seu horror     |                                        \n"+
-					 "                         /   ___________________________________________________________________|                                                                                                                   \n"+
+					 "                          /   ___________________________________________________________________|                                                                                                                   \n"+
 					"                         /                          \\_*_____/                                                                                   \r\n" + 
 					"                                                    /\\*_|__/\\                                                                                   \n"+
 					"░▄████░▀▄░░░░░░░▄▀░█████▄░                         / /\\_|*/\\ \\                                                                                \r\n" +
@@ -173,22 +173,33 @@ public class Ressurrection {
 			sc.nextLine();
 		}
 		static void imgdesafio4() {
-			System.out.println("+\n"
-					+ "				▄█████████▄▄▄\r\n" + 
-					"               █           █\r\n" + 
-					"               █    █   █  █\r\n" + 
-					"               █      █    █\r\n" + 
-					"               ▒▒▒▒▒▒▒▒▒▒▒▒▒\r\n" + 
-					"              ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒\r\n" + 
-					"             ▄█             █▄\r\n" + 
-					"           ▄▀ █       ▀      █▀▄\r\n" + 
-					"      ▀▀▀▄▀   █              █  ▀▄▀▀▀ \r\n" + 
-					"       ▄▀ █   █       ▀      █  █ ▀▄\r\n" + 
-					"          ▀   █              █  ▀\r\n" + 
-					"              █       ▀      █\r\n" + 
-					"              ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀\r\n" + 
-					""
-					);
+			System.out.println("   \n              "
+					+"              _________________________________________________________________________________________________                                                                                                           \n"+
+					"                           |                                                                                                 |  \n"+
+					"                           |   Ola alma perdida, voce veio meio socorrer? estou perdido no deserto e preciso voltar ao meu   |       \n"+
+					"                           | habitart de neve. Se consegui me ajudar ira continuar sua jornada a sua recompensa,             |             \n"+
+					"                           | caso contrario eu irei desaparecerei e vc ficara nesse deserto por toda a eternidade.           |        \n"+
+					"                           | a Charada e encontre a resposta de em quanto tempo irei derreter e o guardiao das almas         |             \n"+
+					"                           | ira permitir a nossa passagem para meu habitart.                                                |          \n"+
+					"                           |  _______________________________________________________________________________________________|          \n"+
+					"                           /                                                                                                             \n"+
+					"                ▄▄▄▄▄▄▄▄▄                         Volume             /                                                                                   \n"+
+					"                █████████                      2.400  |-------------/                                                                                              \n"+
+					"               ███████████▄▄                          |            / |                                                                                \n" + 
+					"               █░░░░░░░░░░░█                          |           /  |                                                                   \r\n" + 
+					"               █░░░░█░░░█░░█                          |          /   |                                                               \r\n" + 
+					"               █░░░░░░█░░░░█                    2.000 |---------/    |                                                               \r\n" + 
+					"               ▓▓▓▓▓▓▓▓▓▓▓▓▓▓                         |       / |    |                                                                  \r\n" + 
+					"              ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓                        |      /  |    |                                                                 \r\n" + 
+					"             ▄█░░░░▓▓▓░▓▓▓░░░█▄                       |     /   |    |                                                                 \r\n" + 
+					"           ▄▀ █░░░░▓▓░▀░▓▓░░░█▀▄                      |    /    |    |                                                                \r\n" + 
+					"      ▀▀▀▄▀   █░░░░░░░░░░░░░░█  ▀▄▀▀▀                 |   /     |    |                                                               \r\n" + 
+					"       ▄▀ █   █░░░░░░░▀░░░░░░█  █ ▀▄                  |  /      |    |                                                             \r\n" + 
+					"          ▀   █░░░░░░░░░░░░░░█  ▀                     | /       |    |                                                         \r\n" + 
+					"              █░░░░░░░▀░░░░░░█                  1.000 |/        |    |                                 \r\n" + 
+					"              ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀                    ____|_________|____|_______________                                                                          \r\n" + 
+					"                                                     0|        50    75         Tempo                                                                                 \n"+
+					"");
 			sc.nextLine();
 		}
 		
