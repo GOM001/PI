5d2ba3d5bb0f5bb826030fde33a00236d6183aed
aurelio1989bispo <55900354+aurelio1989bispo@users.noreply.github.com>
2019-11-07 16:32:18 -0300

    Update RessurrectionA.java

--- Jogo/src/jogo/RessurrectionA.java
+++ Jogo/src/jogo/RessurrectionA.java
@@ -8,7 +8,7 @@ public class RessurrectionA {
 	 
 	static void play4() {
 		int i = 1;
-	List<Integer> resp = new ArrayList<Integer>();
+	ArraysList<Integer> resp = new ArrayList<Integer>();
 	
 	resp.add(20);
 	resp.add(12);
@@ -91,6 +91,87 @@ public class RessurrectionA {
 	
 	
 	}
+	static void play5() {
+		int i = 1;
+		ArraysList<Integer> resp = new ArrayList<Integer>();
+		 resp.add(20);
+		 resp.add(14);
+		 resp.add(22);
+		 resp.add(38);
+		 resp.add(7);
+		 
+	desafio5.flor();
+	do {
+	System.out.println("Ok mortal ansioso pela vida, aqui vai meu desafio: você está em ambiente inóspito que inicialmente  tem 125 bactérias"+
+	"\n Sabendo-se que essa população de bactérias dobra a cada 2 horas, o tempo necessário, em horas, para que o número de bactérias cheguem a 256.000, é igual a: "+
+	"\n a)"+resp.get(0)+"\n b)"+resp.get(1)+"\n c)"+resp.get(2)+"\n d)"+resp.get(3)+"\n e)"+resp.get(4));
+	String escolha = sc.next();
+	switch (escolha) {
+	
+	case "a":
+	case "A":
+		if (resp.get(0)==22) {
+			System.out.println(" Parabéns Voce Acertou! ");
+			i = 3;
+		}
+		else {
+			
+			System.out.println(" Tente novamente!");
+		}
+		break;
+	case "b":
+	case "B":
+		if (resp.get(1)==22) {
+			System.out.println(" Parabéns Voce Acertou! ");
+			i = 3;
+		}
+		else {
+			
+			System.out.println(" Tente novamente!");
+		}
+		break;
+	case "c":	
+	case "C":
+		if (resp.get(2)==22) {
+			System.out.println(" Parabéns Voce Acertou! ");
+			i = 3;
+			}
+		else {
+			
+			System.out.println(" Tente novamente!");
+		}
+		break;
+	
+	case "D" :
+	case "d" :
+		if (resp.get(3)==22) {
+			System.out.println(" Parabéns Voce Acertou! ");
+			i = 3;
+		}
+		else {
+			
+			System.out.println(" Tente novamente!");
+		}break;
+	case "e":
+	case "E":
+		if (resp.get(4)==22) {
+			System.out.println(" Parabéns Voce Acertou! ");
+			i = 3;
+		}
+		else {
+			
+			System.out.println(" Tente novamente!");
+		}
+	
+						
+	default : 
+		
+		System.out.println(" Opcao Invalida!");
+		i = 3;
+	}
+	Collections.shuffle(resp);
+	
+	}while(i<3);
 
 	public static void main(String[] args) {
 	play4();
