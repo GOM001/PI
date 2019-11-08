a4e4615064706d1ec51e730805a48b0cbd5bee0b
aurelio1989bispo <55900354+aurelio1989bispo@users.noreply.github.com>
2019-11-08 13:57:41 -0300

    Update RessurrectionA.java
    Atualização do desafio 2

--- Jogo/src/jogo/RessurrectionA.java
+++ Jogo/src/jogo/RessurrectionA.java
@@ -5,10 +5,22 @@ import java.util.*;
 public class RessurrectionA {
 	
 	static Scanner sc = new Scanner (System.in);
-	 
-	static void play4() {
+	
+	/*#############################################################################################################################################################
+	 * 
+	 * 
+	 * 
+	 * 
+	 *  								Desafio 2
+	 *  
+	 * 
+	 * 
+	 * #############################################################################################################################################################
+	 */		 
+	static void play2() {
 		int i = 1;
-	ArrayList<Integer> resp = new ArrayList<Integer>();
+		boolean gm = true;
+		ArrayList<Integer> resp = new ArrayList<Integer>();
 	
 	resp.add(20);
 	resp.add(12);
@@ -17,8 +29,8 @@ public class RessurrectionA {
 	resp.add(2);
 		
 	do {
-	i = i+1;
-	//desafio4.imgboneco();
+	
+		Ressurrection.imgdesafio4();
 	System.out.println(" Tenho 1.000 quilos de neve e o sol está me fazendo derreter de forma constante."+
 	"\n Em cinquenta minutos mais tarde o calor irá aumentar e o seu tempo diminuir,estarei derretendo mais rápido. veja o quanto tempo tenho e resolva a questão e me ajude a sair desse deserto :"+
 			"\n a)"+resp.get(0)+"\n b)"+resp.get(1)+"\n c)"+resp.get(2)+"\n d)"+resp.get(3)+"\n e)"+resp.get(4));
@@ -30,10 +42,12 @@ public class RessurrectionA {
 				if (resp.get(0)==4) {
 					System.out.println(" Parabéns Voce Acertou! ");
 					i = 3;
+					gm = false;
 				}
 				else {
-					
-					System.out.println(" Tente novamente!");
+					 gm=true;
+					 i=i+1;
+					System.out.println(" Tente novamente! Voce tem mais uma chance !");
 				}
 				break;
 			case "b":
@@ -41,10 +55,12 @@ public class RessurrectionA {
 				if (resp.get(1)==4) {
 					System.out.println(" Parabéns Voce Acertou! ");
 					i = 3;
+					gm = false;
 				}
 				else {
-					
-					System.out.println(" Tente novamente!");
+					gm=true;
+					i=i+1;
+					System.out.println(" Tente novamente! Voce tem mais uma chance !");
 				}
 				break;
 			case "c":	
@@ -52,10 +68,12 @@ public class RessurrectionA {
 				if (resp.get(2)==4) {
 					System.out.println(" Parabéns Voce Acertou! ");
 					i = 3;
+					gm = false;
 					}
 				else {
-					
-					System.out.println(" Tente novamente!");
+					gm=true;
+					i=i+1;
+					System.out.println(" Tente novamente! Voce tem mais uma chance !");
 				}
 				break;
 			
@@ -64,58 +82,90 @@ public class RessurrectionA {
 				if (resp.get(3)==4) {
 					System.out.println(" Parabéns Voce Acertou! ");
 					i = 3;
+					gm = false;
 				}
 				else {
-					
-					System.out.println(" Tente novamente!");
+					gm=true;
+					i=i+1;
+					System.out.println(" Tente novamente! Voce tem mais uma chance !");
 				}break;
 			case "e":
 			case "E":
 				if (resp.get(4)==4) {
 					System.out.println(" Parabéns Voce Acertou! ");
 					i = 3;
+					gm = false;
 				}
 				else {
-					
-					System.out.println(" Tente novamente!");
+					gm=true;
+					i=i+1;;
+					System.out.println(" Tente novamente! Voce tem mais uma chance !");
 				}
 			
 								
 			default : 
 				
 				System.out.println(" Opcao Invalida!");
-				i = 3;
+				i=i+1;
+				gm = true;
 			}
 			Collections.shuffle(resp);
-	}while (i<3);
+	}while (i<=2
+			);
+		if(gm==true) {
+		
+		Ressurrection.gameOver();
+		}
+		else {
+			
+		play5();
+	}
 	
 	
 	}
+	
+	
+	/*#############################################################################################################################################################
+	 * 
+	 * 
+	 * 
+	 * 
+	 *  								Desafio 5
+	 *  
+	 * 
+	 * 
+	 * #############################################################################################################################################################
+	 */	
 	static void play5() {
 		int i = 1;
-		ArrayList<Integer> resp = new ArrayList<Integer>();
+	boolean gm = true;
+		ArrayList<Integer> resp = new ArrayList<Integer>(); // Opcoes de respostas em um arraylist
 		 resp.add(20);
 		 resp.add(14);
 		 resp.add(22);
 		 resp.add(38);
 		 resp.add(7);
 		 
-	desafio5.flor();
+	
 	do {
+	Ressurrection.imgdesafio5();
 	System.out.println("Ok mortal ansioso pela vida, aqui vai meu desafio: você está em ambiente inóspito que inicialmente  tem 125 bactérias"+
 	"\n Sabendo-se que essa população de bactérias dobra a cada 2 horas, o tempo necessário, em horas, para que o número de bactérias cheguem a 256.000, é igual a: "+
 	"\n a)"+resp.get(0)+"\n b)"+resp.get(1)+"\n c)"+resp.get(2)+"\n d)"+resp.get(3)+"\n e)"+resp.get(4));
-	String escolha = sc.next();
-	switch (escolha) {
+	String escolha = sc.next(); // entrada do usuario, opcao escolhida
+	
+	switch (escolha) { 
 	
 	case "a":
 	case "A":
 		if (resp.get(0)==22) {
 			System.out.println(" Parabéns Voce Acertou! ");
 			i = 3;
+			gm = false;
 		}
 		else {
-			
+			gm=true;
+			i=i+1;;
 			System.out.println(" Tente novamente!");
 		}
 		break;
@@ -124,9 +174,11 @@ public class RessurrectionA {
 		if (resp.get(1)==22) {
 			System.out.println(" Parabéns Voce Acertou! ");
 			i = 3;
+			gm = false;
 		}
 		else {
-			
+			gm=true;
+			i=i+1;;
 			System.out.println(" Tente novamente!");
 		}
 		break;
@@ -135,9 +187,11 @@ public class RessurrectionA {
 		if (resp.get(2)==22) {
 			System.out.println(" Parabéns Voce Acertou! ");
 			i = 3;
+			gm = false;
 			}
 		else {
-			
+			gm=true;
+			i=i+1;;
 			System.out.println(" Tente novamente!");
 		}
 		break;
@@ -147,9 +201,11 @@ public class RessurrectionA {
 		if (resp.get(3)==22) {
 			System.out.println(" Parabéns Voce Acertou! ");
 			i = 3;
+			gm = false;
 		}
 		else {
-			
+			gm=true;
+			i=i+1;;
 			System.out.println(" Tente novamente!");
 		}break;
 	case "e":
@@ -157,26 +213,39 @@ public class RessurrectionA {
 		if (resp.get(4)==22) {
 			System.out.println(" Parabéns Voce Acertou! ");
 			i = 3;
+			gm = false;
 		}
 		else {
-			
+			gm=true;
+			i=i+1;;
 			System.out.println(" Tente novamente!");
 		}
 	
 						
 	default : 
-		
+		gm=true;
+		i=i+1;;
 		System.out.println(" Opcao Invalida!");
-		i = 3;
+		
 	}
 	Collections.shuffle(resp);
 	
-	}while(i<3);
+	}while(
+			i<=2);
+	
+		if(gm==true) {
+			
+			Ressurrection.gameOver();
+	}
+		else {
+			
+		}
+	
+	}
 
 	public static void main(String[] args) {
-	play4();
-	play5();	
-	Ressurrection.gameOver();
+	play2();	
+	
 	}
 
 }
