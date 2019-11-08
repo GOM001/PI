cb987c6311ffd24538a190a455d07228327fa858
PedroHenriqueAS <57292535+PedroHenriqueAS@users.noreply.github.com>
2019-11-08 18:35:46 -0300

    update

--- Jogo/src/jogo/Ressurrection.java
+++ Jogo/src/jogo/Ressurrection.java
@@ -293,6 +293,121 @@ public class Ressurrection {
 		if (gm==true){
 			gameover();
 			}
+	//////////////////////////////////////////////// SEGUNDO DESAFIO DO JOGO///////////////////////////////////////////////
+			/*#############################################################################################################################################################
+	 * 
+	 * 
+	 * 
+	 * 
+	 *  								Desafio 2
+	 *  
+	 * 
+	 * 
+	 * #############################################################################################################################################################
+	 */		 
+	static void desafio2() {
+		int i = 1;
+		boolean gm = true;
+		ArrayList<Integer> resp = new ArrayList<Integer>();
+	
+	resp.add(20);
+	resp.add(12);
+	resp.add(6);
+	resp.add(4);
+	resp.add(2);
+		
+	do {
+	
+	imgdesafio4();
+	System.out.println(" Tenho 1.000 quilos de neve e o sol está me fazendo derreter de forma constante."+
+	"\n Em cinquenta minutos mais tarde o calor irá aumentar e o seu tempo diminuir,estarei derretendo mais rápido."+
+	 "\n veja o quanto tempo tenho e resolva a questão e me ajude a sair desse deserto :"+
+	"\n a)"+resp.get(0)+"\n b)"+resp.get(1)+"\n c)"+resp.get(2)+"\n d)"+resp.get(3)+"\n e)"+resp.get(4));
+			String escolha = sc.next();
+			switch (escolha) {
+			
+			case "a":
+			case "A":
+				if (resp.get(0)==4) {
+					System.out.println(" Parabéns Voce Acertou! ");
+					i = 3;
+					gm = false;
+				}
+				else {
+					 gm=true;
+					 i=i+1;
+					System.out.println(" Tente novamente! Voce tem mais uma chance !");
+				}
+				break;
+			case "b":
+			case "B":
+				if (resp.get(1)==4) {
+					System.out.println(" Parabéns Voce Acertou! ");
+					i = 3;
+					gm = false;
+				}
+				else {
+					gm=true;
+					i=i+1;
+					System.out.println(" Tente novamente! Voce tem mais uma chance !");
+				}
+				break;
+			case "c":	
+			case "C":
+				if (resp.get(2)==4) {
+					System.out.println(" Parabéns Voce Acertou! ");
+					i = 3;
+					gm = false;
+					}
+				else {
+					gm=true;
+					i=i+1;
+					System.out.println(" Tente novamente! Voce tem mais uma chance !");
+				}
+				break;
+			
+			case "D" :
+			case "d" :
+				if (resp.get(3)==4) {
+					System.out.println(" Parabéns Voce Acertou! ");
+					i = 3;
+					gm = false;
+				}
+				else {
+					gm=true;
+					i=i+1;
+					System.out.println(" Tente novamente! Voce tem mais uma chance !");
+				}break;
+			case "e":
+			case "E":
+				if (resp.get(4)==4) {
+					System.out.println(" Parabéns Voce Acertou! ");
+					i = 3;
+					gm = false;
+				}
+				else {
+					gm=true;
+					i=i+1;;
+					System.out.println(" Tente novamente! Voce tem mais uma chance !");
+				}
+			
+								
+			default : 
+				
+				System.out.println(" Opcao Invalida!");
+				i=i+1;
+				gm = true;
+			}
+			Collections.shuffle(resp);
+	}while (i<=2
+			);
+		if(gm==true) {
+		
+		gameOver();
+		}
+		else {
+	}
+	}
 	/////////////////////////////////////////////// TERCEIRO DESAFIO DO JOGO //////////////////////////////////////////////
 	static void Desafio3() {
 		String r = "";
